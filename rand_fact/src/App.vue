f4650adf244089f4376ee7a388f17789a3d737a5
ShadowP1e <andrey.bakov.2006@gmail.com>
2020-12-24 18:14:25 +0300

    files

---
+++ rand_fact/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" height=150px width=150px>
+    <helloWorld v-bind:value="helloWorld" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      helloWorld: {
+        fact: '',
+      }
+    }
+  },
+  methods:{
+    getFact(){
+      this.axios.get('http://188.225.47.187/api/cats/random-fact.php')
+        .then((response)=>{
+          this.helloWorld.fact = response.data;
+        })
+    }
+  },
+  mounted(){
+    this.getFact();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ rand_fact/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+ <p>{{value.fact}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['value']
+}
+</script>
+
+<style>
+  .container{
+        width: 400px;
+        margin: auto;
+        border: 1px solid rgb(27, 70, 38);
+        border-radius: 5px;
+        padding: 20px;
+        background: #12ac4d;
+        color: #fff;
+    }
+</style>
